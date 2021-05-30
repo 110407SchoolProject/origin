1c7df363044317f8f46a1c73067bc68b1542a1ae
garywang0827@gmail.com <garywang0827@gmail.com>
2021-05-30 23:56:05 +0800

    新增UpdateDate欄位、Profile的Image可以從手機相簿中存取得相片

--- app/src/main/java/com/example/a110407_app/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/a110407_app/ui/profile/ProfileFragment.java
@@ -1,14 +1,24 @@
 package com.example.a110407_app.ui.profile;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,10 +35,12 @@ import com.example.a110407_app.R;
 import com.example.a110407_app.ui.PasswordSetting;
 import com.example.a110407_app.ui.SQLiteDBHelper;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.lang.Math;
+import java.util.jar.Pack200;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,7 +58,7 @@ public class ProfileFragment extends Fragment {
     SQLiteDBHelper mHelper;
     private final String DB_NAME = "MyDairy.db";
     private String TABLE_NAME = "MyDairy";
-    private final int DB_VERSION = 13;
+    private final int DB_VERSION = 14;
     private ArrayList<HashMap<String, String>> diaryList;
 
     private Button editprofile;
@@ -71,6 +83,10 @@ public class ProfileFragment extends Fragment {
     private EditText getOpenClosePassword; //抓輸入密碼的EditText上的文字
     private String strOpenPassword; //接EditText的文字轉成string的String
     private String p;// 抓密碼表密碼
+
+    private final static int PHOTO = 99 ;// 取得相簿內相片
+    private static final int CAMERA = 60;
+    private DisplayMetrics mPhone;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -171,6 +187,8 @@ public class ProfileFragment extends Fragment {
         remindtext = getView().findViewById(R.id.remindtext);
         remindtext.setText("設置日記提醒");
 
+        mPhone = new DisplayMetrics();
+
         //以下為 Gary 5/28 寫的
         //初始化 TableUser資料庫
         TableUserPassword = new SQLiteDBHelper(getActivity(),DB_NAME,null,DB_VERSION,PASSWORD_TABLE_NAME);
@@ -254,6 +272,89 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getPermissionCamera();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, PHOTO);
+            }
+        });
+
+    }
+
+    //拍照完畢或選取圖片後呼叫此函式
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,Intent data)
+    {
+        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
+        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
+        {
+            //取得照片路徑uri
+            Uri uri = data.getData();
+            ContentResolver cr = getActivity().getContentResolver();
+
+            try {
+                //讀取照片，型態為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
+                if(bitmap.getWidth()>bitmap.getHeight()){
+                    ScalePic(bitmap, mPhone.heightPixels);
+                }
+                else {
+                    ScalePic(bitmap,mPhone.widthPixels);
+
+                }
+            }catch (FileNotFoundException e) {
+                //
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    private void ScalePic(Bitmap bitmap,int phone)
+    {
+        //縮放比例預設為1
+        float mScale = 1 ;
+
+        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
+        if(bitmap.getWidth() > phone )
+        {
+            //判斷縮放比例
+            mScale = (float)phone/(float)bitmap.getWidth();
+
+            Matrix mMat = new Matrix() ;
+            mMat.setScale(mScale, mScale);
+
+            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap,
+                    0,
+                    0,
+                    bitmap.getWidth(),
+                    bitmap.getHeight(),
+                    mMat,
+                    false);
+            //image.setAdjustViewBounds(true);
+
+            image.setImageBitmap(mScaleBitmap);
+        }
+        else {
+            //image.setAdjustViewBounds(true);
+            image.setImageBitmap(bitmap);
+        }
+    }
+
+
+
+
+    public void getPermissionCamera(){
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.CAMERA},1);
+
+        }
     }
 
 
