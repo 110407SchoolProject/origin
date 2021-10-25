73346aa0f296afbbac9a6efce6c640c61d622a23
PonyWeng <pony71909@gmail.com>
2021-10-25 16:04:34 +0800

    設定連線逾時時間
    因為bert預測要一段時間 所以我把連線時間拉長 ->60秒

--- app/src/main/java/com/example/a110407_app/RetrofitAPI/RetrofitManager.java
+++ app/src/main/java/com/example/a110407_app/RetrofitAPI/RetrofitManager.java
@@ -1,5 +1,8 @@
 package com.example.a110407_app.RetrofitAPI;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,13 +12,18 @@ public class RetrofitManager {
     private static RetrofitManager mInstance = new RetrofitManager();
 
     private APIService ourAPIService;
-
+    OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .build();
     private RetrofitManager() {
 
         // 設置baseUrl即要連的網站，addConverterFactory用Gson作為資料處理Converter
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://server.gywang.io:8084/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         ourAPIService = retrofit.create(APIService.class);
