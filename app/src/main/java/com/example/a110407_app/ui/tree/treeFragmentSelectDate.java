ac03baf55f64700d7f0b93caf1a197e870fa657c
PonyWeng <pony71909@gmail.com>
2021-11-13 19:37:10 +0800

    調整navBar至心情樹日期選擇畫面的邏輯

--- app/src/main/java/com/example/a110407_app/ui/tree/treeFragmentSelectDate.java
+++ app/src/main/java/com/example/a110407_app/ui/tree/treeFragmentSelectDate.java
@@ -1,14 +1,22 @@
 package com.example.a110407_app.ui.tree;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.a110407_app.EditDiaryActivity;
 import com.example.a110407_app.R;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +33,8 @@ public class treeFragmentSelectDate extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnTreeGenerate;
 
     public treeFragmentSelectDate() {
         // Required empty public constructor
@@ -55,6 +65,8 @@ public class treeFragmentSelectDate extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -62,5 +74,30 @@ public class treeFragmentSelectDate extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tree_select_date, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        System.out.println("你正在treeFragment ");
+        System.out.println(getActivity());
+
+                btnTreeGenerate=getActivity().findViewById(R.id.btnTreeGenerateInTree);
+        btnTreeGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello 你按下了產生心情樹");
+
+                //透過當Bundle當傳遞參數的容器
+                Bundle bundle = new Bundle();
+                bundle.putString("startDate","1999-12-07");
+                bundle.putString("endDate","2021-12-07");
+
+                //傳入
+                NavHostFragment.findNavController(treeFragmentSelectDate.this)
+                        .navigate(R.id.nav_treeGenerated,bundle);
+            }
+        });
     }
 }
